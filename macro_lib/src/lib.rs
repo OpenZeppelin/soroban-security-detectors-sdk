b8cbc1307d637881629348d0b88642c08b3ae753
Georgii Plotnikov <accembler@gmail.com>
2024-11-12 11:22:28 +0900

    Enhance node_location macro documentation to clarify usage and panic conditions for struct implementation

--- macro_lib/src/lib.rs
+++ macro_lib/src/lib.rs
@@ -5,9 +5,13 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Ident, ItemStruct, LitStr};
 
+/// This macro generates the implementation of the `NodeLocation` trait for a struct.
+/// The struct must have a field that is a `syn::Item`, which is the item that the struct represents.
+///
+/// # Panics
+/// This macro will panic if the struct does not have a field that is a `syn::Item`.
 #[proc_macro_attribute]
 pub fn node_location(args: TokenStream, input: TokenStream) -> TokenStream {
-    // Parse the input struct
     let input = parse_macro_input!(input as ItemStruct);
     let struct_name = &input.ident;
     let mut inner_field_name: Ident = Ident::new("inner_item", proc_macro2::Span::call_site());
